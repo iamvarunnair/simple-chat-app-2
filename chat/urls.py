c8fea6c42bc52a51acf7d14e0bf7afc23494f314
iamvarunnair <imvarunnair@gmail.com>
2020-08-04 15:35:15 +0530

    adding default url '/' path

--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,13 @@
 # chat/urls.py
 from django.urls import path
 from . import views
+from django.conf.urls import url
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('login', views.login, name='login'),
     path('home', views.home, name='home'),
     path('<str:room_name>/', views.room, name='room'),
     path('logout', views.logout, name='logout'),
+    url(r'^$', RedirectView.as_view(pattern_name="login")),
 ]
